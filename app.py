d851c0d62db61463c383d386c37ed25146525a9c
KostyaTu <tulubkosta@gmail.com>
2026-04-03 13:26:47 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+‎import json
+‎import os
+‎
+‎app = Flask(__name__)
+‎
+app.secret_key = 'твой_секретный_ключ_например_12345'
+
+‎def load_users():
+‎    if not os.path.exists('users.json'):
+‎        return {}
+‎    with open('users.json', 'r', encoding='utf-8') as f:
+‎        return json.load(f)
+‎
+‎def save_users(users):
+‎    with open('users.json', 'w', encoding='utf-8') as f:
+‎        json.dump(users, f, indent=4, ensure_ascii=False)
+‎
+‎@app.route('/')
+def index():
+    return render_template('index.html')
+‎
+‎@app.route('/register')
+‎def register_page():
+‎    return render_template('register.html')
+‎
+‎@app.route('/do_register', methods=['POST'])
+‎def do_register():
+‎    login = request.form.get('login', '').strip()
+‎    password = request.form.get('password', '').strip()
+‎    
+‎    if not login or not password:
+‎        return "Ошибка: логин и пароль не могут быть пустыми!"
+‎    
+‎    users = load_users()
+‎    if login in users:
+‎        return f"Ошибка: пользователь {login} уже существует!"
+‎    
+‎    users[login] = password
+‎    save_users(users)
+‎    return f"Регистрация успешна! <a href='{url_for('login_page')}'>Войти</a>"
+‎
+‎@app.route('/login')
+‎def login_page():
+‎    return render_template('login.html')
+‎
+@app.route('/do_login', methods=['POST'])
+def do_login():
+    login = request.form.get('login', '').strip()
+    password = request.form.get('password', '').strip()
+    
+    users = load_users()
+    if login in users and users[login] == password:
+        session['username'] = login   # запоминаем, что пользователь вошёл
+        return redirect(url_for('account'))
+    else:
+        return "Неверный логин или пароль."
+‎    
+‎    users = load_users()
+‎    if login in users and users[login] == password:
+‎        return f"Добро пожаловать, {login}! <a href='{url_for('register_page')}'>На главную</a>"
+‎    else:
+‎        return "Неверный логин или пароль."
+‎
+@app.route('/account')
+def account():
+    if 'username' not in session:
+        return redirect(url_for('login_page'))  # если не залогинен, отправляем на вход
+    return render_template('account.html', username=session['username'])
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)   # удаляем логин из сессии
+    return redirect(url_for('index'))
+
+‎if __name__ == '__main__':
+‎    app.run(debug=True)
